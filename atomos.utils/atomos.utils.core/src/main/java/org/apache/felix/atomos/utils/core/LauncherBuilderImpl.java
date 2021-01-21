bcc146b11500fcf0c9c8edc72c0fcbc96ce01a4a
Thomas Watson <tjwatson@us.ibm.com>
2021-01-21 10:30:17 -0600

    Use parent felix project parent pom

--- atomos.utils/atomos.utils.core/src/main/java/org/apache/felix/atomos/utils/core/LauncherBuilderImpl.java
+++ atomos.utils/atomos.utils.core/src/main/java/org/apache/felix/atomos/utils/core/LauncherBuilderImpl.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.felix.atomos.utils.core;
 
 import java.lang.reflect.Method;
@@ -7,12 +20,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import aQute.bnd.annotation.spi.ServiceProvider;
 import org.apache.felix.atomos.utils.api.Launcher;
 import org.apache.felix.atomos.utils.api.LauncherBuilder;
 import org.apache.felix.atomos.utils.api.plugin.SubstratePlugin;
 import org.osgi.util.converter.Converter;
 import org.osgi.util.converter.Converters;
+
+import aQute.bnd.annotation.spi.ServiceProvider;
 
 @ServiceProvider(LauncherBuilder.class)
 public class LauncherBuilderImpl implements LauncherBuilder
